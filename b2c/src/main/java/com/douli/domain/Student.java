023b78da87374e155a6dd3a48036e7884924e2f6
webber678@163.com <ice_run520@163.com>
2019-10-21 13:21:02 +0800

    创建Student

---
+++ b2c/src/main/java/com/douli/domain/Student.java
@@ -0,0 +1,11 @@
+package com.douli.domain;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String id;
+    private String name;
+    private String idNo;
+    private String idType;
+}
